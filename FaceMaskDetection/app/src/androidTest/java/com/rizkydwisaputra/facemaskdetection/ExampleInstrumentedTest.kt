b6b4d8288947e9bbebce087089649c19d644314e
MRIZQIFAUZIMAKSUM <83562603+MRIZQIFAUZIMAKSUM@users.noreply.github.com>
2021-12-12 23:20:20 +0700

    INIT

--- FaceMaskDetection/app/src/androidTest/java/com/rizkydwisaputra/facemaskdetection/ExampleInstrumentedTest.kt
+++ FaceMaskDetection/app/src/androidTest/java/com/rizkydwisaputra/facemaskdetection/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rizkydwisaputra.facemaskdetection", appContext.packageName)
+        assertEquals("com.MRIZQIFAUZIMAKSUM.facemaskdetection", appContext.packageName)
     }
 }
--- fragment/app/src/androidTest/java/com/rizkydwisaputra/pindahfragment/ExampleInstrumentedTest.kt
+++ fragment/app/src/androidTest/java/com/rizkydwisaputra/pindahfragment/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rizkydwisaputra.pindahfragment", appContext.packageName)
+        assertEquals("com.MRIZQIFAUZIMAKSUM.pindahfragment", appContext.packageName)
     }
 }
